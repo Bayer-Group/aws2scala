4eb6f2992fe010038408a941922d62e8f860fdcc
Daniel Solano Gómez <daniel@solanogomez.org>
2016-02-25 09:52:57 -0600

    Make AdaptableScalaFutures actually work

--- core/src/test/scala/com/monsanto/arch/awsutil/test/AdaptableScalaFutures.scala
+++ core/src/test/scala/com/monsanto/arch/awsutil/test/AdaptableScalaFutures.scala
@@ -7,16 +7,18 @@ import org.slf4j.LoggerFactory
 import scala.concurrent.duration.DurationInt
 
 trait AdaptableScalaFutures extends ScalaFutures {
-  override implicit lazy val patienceConfig =
+  private lazy val lazyConfig =
     if (Option(System.getenv("TRAVIS")).contains("true")) {
       AdaptableScalaFutures.logger.info("Using Travis patience configuration")
-      PatienceConfig(5.seconds, 50.milliseconds)
+      PatienceConfig(10.seconds, 50.milliseconds)
     } else {
       AdaptableScalaFutures.logger.info("Using default patience configuration")
-      PatienceConfig(200.milliseconds, 10.milliseconds)
+      PatienceConfig(250.milliseconds, 10.milliseconds)
     }
+
+  override implicit def patienceConfig = lazyConfig
 }
 
-object AdaptableScalaFutures extends ScalaFutures {
+object AdaptableScalaFutures extends AdaptableScalaFutures {
   private val logger: Logger = Logger(LoggerFactory.getLogger("AdaptableScalaFutures"))
 }
