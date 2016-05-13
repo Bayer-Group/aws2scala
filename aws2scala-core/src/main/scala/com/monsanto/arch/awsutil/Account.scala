7c7a8245a00b45c437750f6d3dc42df8017ecd58
Daniel Solano Gómez <daniel@solanogomez.org>
2016-05-13 16:47:40 -0500

    Allow "aws" to be an account ID

--- aws2scala-core/src/main/scala/com/monsanto/arch/awsutil/Account.scala
+++ aws2scala-core/src/main/scala/com/monsanto/arch/awsutil/Account.scala
@@ -4,11 +4,11 @@ import com.monsanto.arch.awsutil.partitions.Partition
 
 /** Represents an AWS account.
   *
-  * @param id the AWS account ID, which must be a 12-digit string with no hyphens
+  * @param id the AWS account ID, which must be a 12-digit string with no hyphens or the string `"aws"`
   * @param partition the partition to which the account belongs, defaults to the global AWS partition
   */
 case class Account(id: String, partition: Partition = Partition.Aws) {
-  require(id.matches("^[0-9]{12}$"), "An AWS account ID must be a 12-digit string.")
+  require(id.matches("^[0-9]{12}|aws$"), "An AWS account ID must be a 12-digit string.")
 
   /** Returns the ARN that corresponds to this account. */
   def arn: AccountArn = AccountArn(this)
