45ac6fcd7feccd43821ed0ff942cda5892d07f18
Daniel Solano Gómez <daniel@solanogomez.org>
2016-06-12 06:28:34 -0500

    Add OO support to ManagedPolicyVersion
    It can get itself, set itself as the default version, and delete itself.

--- aws2scala-iam-tests/src/test/scala/com/monsanto/arch/awsutil/identitymanagement/model/ManagedPolicyVersionSpec.scala
+++ aws2scala-iam-tests/src/test/scala/com/monsanto/arch/awsutil/identitymanagement/model/ManagedPolicyVersionSpec.scala
@@ -1,16 +1,63 @@
 package com.monsanto.arch.awsutil.identitymanagement.model
 
+import akka.Done
+import akka.stream.Materializer
 import com.monsanto.arch.awsutil.converters.IamConverters._
+import com.monsanto.arch.awsutil.identitymanagement.AsyncIdentityManagementClient
+import com.monsanto.arch.awsutil.test_support.AdaptableScalaFutures._
+import com.monsanto.arch.awsutil.test_support.Materialised
 import com.monsanto.arch.awsutil.testkit.IamScalaCheckImplicits._
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
 import org.scalatest.prop.GeneratorDrivenPropertyChecks._
 
-class ManagedPolicyVersionSpec extends FreeSpec {
+import scala.concurrent.Future
+
+class ManagedPolicyVersionSpec extends FreeSpec with Materialised with MockFactory {
   "a ManagedPolicyVersion should" - {
     "round-trip via its AWS equivalent" in {
       forAll { policyVersion: ManagedPolicyVersion ⇒
         policyVersion.asAws.asScala(policyVersion.policyArn) shouldBe policyVersion
+      }
+    }
+
+    "get itself" - {
+      forAll { (version1: ManagedPolicyVersion, version2: ManagedPolicyVersion) ⇒
+        implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+        (client.getPolicyVersion(_: PolicyArn, _: String)(_: Materializer))
+          .expects(version1.policyArn, version1.versionId, materialiser)
+          .returning(Future.successful(version2))
+
+        val result = version1.refresh().futureValue
+        result shouldBe version2
+      }
+    }
+
+    "set itself the default version" - {
+      forAll { version: ManagedPolicyVersion ⇒
+        implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+        (client.setDefaultPolicyVersion(_: PolicyArn, _: String)(_: Materializer))
+          .expects(version.policyArn, version.versionId, materialiser)
+          .returning(Future.successful(Done))
+
+        val result = version.setAsDefault().futureValue
+        result shouldBe Done
+      }
+    }
+
+    "delete itself" - {
+      forAll { version: ManagedPolicyVersion ⇒
+        implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+        (client.deletePolicyVersion(_: PolicyArn, _: String)(_: Materializer))
+          .expects(version.policyArn, version.versionId, materialiser)
+          .returning(Future.successful(Done))
+
+        val result = version.delete().futureValue
+        result shouldBe Done
       }
     }
   }
--- aws2scala-iam/src/main/scala/com/monsanto/arch/awsutil/identitymanagement/model/ManagedPolicyVersion.scala
+++ aws2scala-iam/src/main/scala/com/monsanto/arch/awsutil/identitymanagement/model/ManagedPolicyVersion.scala
@@ -2,7 +2,12 @@ package com.monsanto.arch.awsutil.identitymanagement.model
 
 import java.util.Date
 
+import akka.Done
+import akka.stream.Materializer
 import com.monsanto.arch.awsutil.auth.policy.Policy
+import com.monsanto.arch.awsutil.identitymanagement.AsyncIdentityManagementClient
+
+import scala.concurrent.Future
 
 /** Contains information about a version of a managed policy.
   *
@@ -17,4 +22,18 @@ case class ManagedPolicyVersion(policyArn: PolicyArn,
                                 document: Option[Policy],
                                 versionId: String,
                                 isDefaultVersion: Boolean,
-                                created: Date)
+                                created: Date) {
+  /** Retrieves a new copy of this policy version.  This should have the
+    * additional effect of retrieving the document.
+    */
+  def refresh()(implicit m: Materializer, client: AsyncIdentityManagementClient): Future[ManagedPolicyVersion] =
+    client.getPolicyVersion(policyArn, versionId)
+
+  /** Sets this version of the managed policy as the default (operational) version. */
+  def setAsDefault()(implicit m: Materializer, client: AsyncIdentityManagementClient): Future[Done] =
+    client.setDefaultPolicyVersion(policyArn, versionId)
+
+  /** Deletes this version of the managed policy. */
+  def delete()(implicit m: Materializer, client: AsyncIdentityManagementClient): Future[Done] =
+    client.deletePolicyVersion(policyArn, versionId)
+}
