85d70a3550380b2a413672a32dfebd9ab38e19d6
Daniel Solano Gómez <daniel@solanogomez.org>
2016-07-05 16:07:40 -0500

    Add OO interface to Role
    A `Role` can now:
    
    * Delete itself
    * Manage its attached policies

--- aws2scala-iam-tests/src/test/scala/com/monsanto/arch/awsutil/identitymanagement/model/RoleSpec.scala
+++ aws2scala-iam-tests/src/test/scala/com/monsanto/arch/awsutil/identitymanagement/model/RoleSpec.scala
@@ -1,30 +1,108 @@
 package com.monsanto.arch.awsutil.identitymanagement.model
 
+import akka.Done
+import akka.stream.Materializer
 import com.amazonaws.services.identitymanagement.{model ⇒ aws}
 import com.monsanto.arch.awsutil.converters.IamConverters._
+import com.monsanto.arch.awsutil.identitymanagement.AsyncIdentityManagementClient
+import com.monsanto.arch.awsutil.test_support.AdaptableScalaFutures._
+import com.monsanto.arch.awsutil.test_support.Materialised
+import com.monsanto.arch.awsutil.testkit.CoreScalaCheckImplicits._
 import com.monsanto.arch.awsutil.testkit.IamScalaCheckImplicits._
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
 import org.scalatest.prop.GeneratorDrivenPropertyChecks._
 
-class RoleSpec extends FreeSpec {
-  "a Role can be round-tripped" - {
-    "from its AWS equivalent" in {
-      forAll { role: Role ⇒
-        val awsRole = new aws.Role()
-          .withArn(role.arn.arnString)
-          .withRoleName(role.name)
-          .withRoleId(role.id)
-          .withPath(role.path.pathString)
-          .withAssumeRolePolicyDocument(role.assumeRolePolicyDocument.toJson)
-          .withCreateDate(role.created)
-        awsRole.asScala.asAws shouldBe awsRole
+import scala.concurrent.Future
+
+class RoleSpec extends FreeSpec with MockFactory with Materialised {
+  "a Role can" - {
+    "be round-trip" - {
+      "from its AWS equivalent" in {
+        forAll { role: Role ⇒
+          val awsRole = new aws.Role()
+            .withArn(role.arn.arnString)
+            .withRoleName(role.name)
+            .withRoleId(role.id)
+            .withPath(role.path.pathString)
+            .withAssumeRolePolicyDocument(role.assumeRolePolicyDocument.toJson)
+            .withCreateDate(role.created)
+          awsRole.asScala.asAws shouldBe awsRole
+        }
+      }
+
+      "via its AWS equivalent" in {
+        forAll { role: Role ⇒
+          role.asAws.asScala shouldBe role
+        }
       }
     }
 
-    "via its AWS equivalent" in {
+    "delete itself" in {
       forAll { role: Role ⇒
-        role.asAws.asScala shouldBe role
+        implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+        (client.deleteRole(_: String)(_: Materializer))
+          .expects(role.name, materialiser)
+          .returning(Future.successful(Done))
+
+        val result = role.delete().futureValue
+        result shouldBe Done
+      }
+    }
+
+    "attach a managed policy" in {
+      forAll { (role: Role, policyArn: PolicyArn) ⇒
+        implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+        (client.attachRolePolicy(_: String, _: PolicyArn)(_: Materializer))
+          .expects(role.name, policyArn, materialiser)
+          .returning(Future.successful(Done))
+
+        val result = role.attachPolicy(policyArn).futureValue
+        result shouldBe Done
+      }
+    }
+
+    "detach a managed policy" in {
+      forAll { (role: Role, policyArn: PolicyArn) ⇒
+        implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+        (client.detachRolePolicy(_: String, _: PolicyArn)(_: Materializer))
+          .expects(role.name, policyArn, materialiser)
+          .returning(Future.successful(Done))
+
+        val result = role.detachPolicy(policyArn).futureValue
+        result shouldBe Done
+      }
+    }
+
+    "list attached managed policies" - {
+      "all of them" in {
+        forAll { (role: Role, expected: Seq[AttachedPolicy]) ⇒
+          implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+          (client.listAttachedRolePolicies(_: String)(_: Materializer))
+            .expects(role.name, materialiser)
+            .returning(Future.successful(expected))
+
+          val result = role.listAttachedPolicies().futureValue
+          result shouldBe expected
+        }
+      }
+
+      "with a prefix" in {
+        forAll { (role: Role, prefix: Path, expected: Seq[AttachedPolicy]) ⇒
+          implicit val client = mock[AsyncIdentityManagementClient]("client")
+
+          (client.listAttachedRolePolicies(_: String, _: Path)(_: Materializer))
+            .expects(role.name, prefix, materialiser)
+            .returning(Future.successful(expected))
+
+          val result = role.listAttachedPolicies(prefix).futureValue
+          result shouldBe expected
+        }
       }
     }
   }
--- aws2scala-iam/src/main/scala/com/monsanto/arch/awsutil/identitymanagement/model/Role.scala
+++ aws2scala-iam/src/main/scala/com/monsanto/arch/awsutil/identitymanagement/model/Role.scala
@@ -2,7 +2,12 @@ package com.monsanto.arch.awsutil.identitymanagement.model
 
 import java.util.Date
 
+import akka.Done
+import akka.stream.Materializer
 import com.monsanto.arch.awsutil.auth.policy.Policy
+import com.monsanto.arch.awsutil.identitymanagement.AsyncIdentityManagementClient
+
+import scala.concurrent.Future
 
 /** Contains information about an IAM role.
   *
@@ -18,4 +23,26 @@ case class Role(arn: RoleArn,
                 path: Path,
                 id: String,
                 assumeRolePolicyDocument: Policy,
-                created: Date)
+                created: Date) {
+  /** Requests deletion of this role.  It must not have any policies attached. */
+  def delete()(implicit m: Materializer, client: AsyncIdentityManagementClient): Future[Done] =
+    client.deleteRole(name)
+
+  /** Attaches the managed policy to this role. */
+  def attachPolicy(policyArn: PolicyArn)
+                  (implicit m: Materializer, client: AsyncIdentityManagementClient): Future[Done] =
+    client.attachRolePolicy(name, policyArn)
+
+  /** Detaches the managed policy from this role. */
+  def detachPolicy(policyArn: PolicyArn)
+                  (implicit m: Materializer, client: AsyncIdentityManagementClient): Future[Done] =
+    client.detachRolePolicy(name, policyArn)
+
+  /** Lists all managed policies attached to this role. */
+  def listAttachedPolicies()(implicit m: Materializer, client: AsyncIdentityManagementClient): Future[Seq[AttachedPolicy]] =
+    client.listAttachedRolePolicies(name)
+
+  /** Lists all managed policies attached to this role that match the given path prefix. */
+  def listAttachedPolicies(prefix: Path)(implicit m: Materializer, client: AsyncIdentityManagementClient): Future[Seq[AttachedPolicy]] =
+    client.listAttachedRolePolicies(name, prefix)
+}
